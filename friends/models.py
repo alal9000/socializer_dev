e30643cb968460a5f46ae44a8dbe3cea06c2fb85
Aaron Lal <lucidlear@gmail.com>
2024-06-02 10:52:07 +1000

    added some changes to friends page

--- friends/models.py
+++ friends/models.py
@@ -7,5 +7,11 @@ class Friend(models.Model):
     receiver = models.ForeignKey(Profile,  on_delete=models.CASCADE, null=True, related_name="receiver")
     status = models.CharField(max_length=10, null=True, choices=[('pending', 'Pending'), ('accepted', 'Accepted'), ('denied', 'Denied')], default='pending')
 
+    def get_other_profile(self, current_profile):
+        if self.sender == current_profile:
+            return self.receiver
+        else:
+            return self.sender
+
     def __str__(self):
         return f"{self.sender} -> {self.receiver}"
--- friends/views.py
+++ friends/views.py
@@ -1,51 +1,62 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
 
 from app.decorators import check_profile_id
 from django.contrib.auth.decorators import login_required
-from . models import Friend
+from .models import Friend
 from app.models import Profile
 
-# Create your views here.
+
+@login_required
 def friends(request, profile_id):
     profile = get_object_or_404(Profile, id=profile_id)
 
-    friends = Friend.objects.filter(sender=profile, status='accepted') | Friend.objects.filter(receiver=profile, status='accepted') 
+    friends = Friend.objects.filter(
+        Q(sender=profile, status="accepted") | Q(receiver=profile, status="accepted")
+    )
 
-    return render(request, 'friends/friends.html', {"friends": friends})
+    other_friends = [
+        friend.get_other_profile(profile).user.first_name for friend in friends
+    ]
+
+    return render(request, "friends/friends.html", {"friends": other_friends, "profile": profile})
+
 
 @login_required
 @check_profile_id
-def friend_requests(request, profile_id ):
+def friend_requests(request, profile_id):
     profile = get_object_or_404(Profile, id=profile_id)
     if request.method == "POST":
-        friend_request_id = request.POST.get('friend_request_id')
-        action = request.POST.get('action')
+        friend_request_id = request.POST.get("friend_request_id")
+        action = request.POST.get("action")
 
         if friend_request_id and action:
-            friend_request = get_object_or_404(Friend, id=friend_request_id, receiver=request.user.profile)
+            friend_request = get_object_or_404(
+                Friend, id=friend_request_id, receiver=request.user.profile
+            )
 
             if action == "approve":
-                friend_request.status = 'accepted'
+                friend_request.status = "accepted"
                 friend_request.save()
                 messages.success(request, "Friend request approved.")
             elif action == "deny":
-                friend_request.status = 'denied'
+                friend_request.status = "denied"
                 friend_request.save()
                 messages.success(request, "Friend request denied.")
-        
-        return redirect('profile', profile_id)
+
+        return redirect("profile", profile_id)
 
     # profile = get_object_or_404(Profile, id=profile_id)
 
     # print("profile is: ", profile)
 
-    requests = Friend.objects.filter(status='pending', receiver=request.user.profile)
+    requests = Friend.objects.filter(status="pending", receiver=request.user.profile)
 
     print("queryset is: ", requests)
 
     context = {
-        'requests': requests,
+        "requests": requests,
     }
 
-    return render(request, 'friends/friend_requests.html', context)
+    return render(request, "friends/friend_requests.html", context)
